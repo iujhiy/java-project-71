3f817ff5ece95053d5478edd117e33d906f99b08
nikita <raknubov2@gmail.con>
2025-07-13 17:19:32 +0300

    fixing some mistakes

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,6 +1,5 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.formatters.JsonFormatter;
@@ -14,23 +13,30 @@ import java.util.Map;
 import java.util.ArrayList;
 
 public class Differ {
-    public static String generate(Map<String, Object> firstFileData,
-                                  Map<String, Object> secondFileData,
-                                  String formatName) throws JsonProcessingException {
+    public static String generate(String filepath1,
+                                  String filepath2,
+                                  String formatName) throws Exception {
         var resultFileDiffers = new ArrayList<String>();
+        var firstFileData = getData(filepath1);
+        var secondFileData = getData(filepath2);
+        var result =  Stylish.generate(firstFileData, secondFileData, resultFileDiffers);
         if (formatName.equals("plain")) {
-            return Plain.generate(firstFileData, secondFileData, resultFileDiffers);
+            result = Plain.generate(firstFileData, secondFileData, resultFileDiffers);
         } else if (formatName.equals("json")) {
-            return JsonFormatter.generate(firstFileData, secondFileData);
-        } else {
-            return Stylish.generate(firstFileData, secondFileData, resultFileDiffers);
+            result = JsonFormatter.generate(firstFileData, secondFileData);
         }
+        System.out.println(result);
+        return result;
     }
 
-    public static String generate(Map<String, Object> firstFileData,
-                                  Map<String, Object> secondFileData) {
+    public static String generate(String filepath1,
+                                  String filepath2) throws Exception {
         var resultFileDiffers = new ArrayList<String>();
-        return Stylish.generate(firstFileData, secondFileData, resultFileDiffers);
+        var firstFileData = getData(filepath1);
+        var secondFileData = getData(filepath2);
+        var result = Stylish.generate(firstFileData, secondFileData, resultFileDiffers);
+        System.out.println(result);
+        return result;
     }
 
     public static Map<String, Object> getData(String filepath) throws Exception {
--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,25 +1,16 @@
 package hexlet.code;
 
-import static hexlet.code.Differ.getData;
+import static hexlet.code.Differ.generate;
 
 public class Formatter {
     public static String chooseFormate(String filepath1, String filepath2,
                                      String formatName) throws Exception {
         if (formatName.equals("plain")) {
-            return differFiles(filepath1, filepath2, "plain");
+            return generate(filepath1, filepath2, "plain");
         } else if (formatName.equals("json")) {
-            return differFiles(filepath1, filepath2, "json");
+            return generate(filepath1, filepath2, "json");
         } else {
-            return differFiles(filepath1, filepath2, "stylish");
+            return generate(filepath1, filepath2, "stylish");
         }
-    }
-
-    private static String differFiles(String filepath1, String filepath2, String formatName)
-            throws Exception {
-        var firstFileData = getData(filepath1);
-        var secondFileData = getData(filepath2);
-        var differResultFiles = Differ.generate(firstFileData, secondFileData, formatName);
-        System.out.println(differResultFiles);
-        return differResultFiles;
     }
 }
